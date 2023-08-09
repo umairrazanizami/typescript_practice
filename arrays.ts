cdaa7543423e50a04c042c4e6ce232efadb6521b
UMAIRRAZANIZAMI <UMAIRRAZANIZAMI@GMAIL.COM>
2023-08-09 23:13:49 +0500

    Arrays in Typescript

--- arrays.ts
+++ arrays.ts
@@ -1,4 +1,5 @@
 import chalk from "chalk";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
 let fruitsName: string[] ;
 
@@ -10,3 +11,29 @@ for (let i in fruitsName ){
 
     console.log(chalk.blue(fruitsName[i]));
 }
+
+// let a :any[] = [1,"2",3,'4',5]
+
+// for (let i in a ){
+
+//     console.log(chalk.red(a[i]));
+// }
+
+console.log(chalk.bgCyan.bold(`The Length of Fruits array is ${fruitsName.length}`));
+
+fruitsName[5] = 'guava';
+
+for (let i in fruitsName ){
+
+    console.log(chalk.red(fruitsName[i]));
+}
+
+console.log(chalk.bgCyan.bold(`The Length of Fruits array is ${fruitsName.length}`));
+console.log('Last Fruit is poping........')
+console.log(fruitsName.pop())
+for (let i in fruitsName ){
+
+    console.log(chalk.red(fruitsName[i]));
+}
+
+console.log(chalk.bgCyan.bold(`The Length of Fruits array is ${fruitsName.length}`));
